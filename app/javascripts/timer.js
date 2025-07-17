9558373e23ceed0a53fd073e86edc70f7d6d9767
Jack Uher <149542675+uherj2@users.noreply.github.com>
2025-07-17 16:00:16 -0400

    Alignment Update
    Start Button stays in center

--- app/javascripts/timer.js
+++ app/javascripts/timer.js
@@ -1,7 +1,15 @@
 const counterSpan = document.getElementById('counter');
 const playButton = document.getElementById('play');
-const skipButton = document.getElementById('skipButton');
 const ringSound = new Audio('/audio/timerRing.mp3');
+
+const timeControlContainer = document.getElementById('timeControlContainer');
+const skipButton = document.createElement('button');
+const alignmentButton = document.createElement('button');
+skipButton.textContent = '>';
+alignmentButton.textContent = '>';
+skipButton.classList.add('skipButton');
+alignmentButton.classList.add('skipButton', 'alignmentButton');
+
 
 let timer
 var time = 1500; //25 minutes
@@ -28,11 +36,13 @@ function startTimer(){
     } else {
         pauseTimer();
     }
+    updateStartButton();
 }
 
 function pauseTimer(){
     isRunning = false;
     clearInterval(timer);
+    updateStartButton();
 }
 
 function stopTimer(){
@@ -43,15 +53,33 @@ function stopTimer(){
     clearInterval(timer);
     switchTimer();
     counterSpan.textContent = formatTime(time);
+    updateStartButton();
+}
+
+let isStartButtonOn = false;
+
+function updateStartButton(){
+    if(isRunning) {
+        isStartButtonOn = true;
+        playButton.classList.add('on'); 
+        playButton.textContent = "Pause"  
+        timeControlContainer.appendChild(skipButton);
+        timeControlContainer.insertBefore(alignmentButton, playButton);
+        
+     } else {
+        isStartButtonOn = false;
+        playButton.classList.remove('on');
+        playButton.textContent = "Start"
+        skipButton.remove();
+        alignmentButton.remove();
+    }
 }
 
 playButton.addEventListener('click', () => {
     startTimer();
-    
-
 });
 
 skipButton.addEventListener('click', () =>  {
     stopTimer();
-})
+});
 
