7126116b895bab1324829b8fba8354e915fdbf3f
Jack Uher <149542675+uherj2@users.noreply.github.com>
2025-08-21 16:02:09 -0400

    Update KeepAwake

--- app/javascripts/keepScreenOn.js
+++ app/javascripts/keepScreenOn.js
@@ -1,19 +1,23 @@
-if ("wakeLock" in navigator) {
-  isSupported = true;
-  statusElem.textContent = "Screen Wake Lock API supported!";
-} else {
-  wakeButton.disabled = true;
-  statusElem.textContent = "Wake lock is not supported by this browser.";
-}
-
-// Create a reference for the Wake Lock.
-let wakeLock = null;
-
 // create an async function to request a wake lock
-try {
-  wakeLock = await navigator.wakeLock.request("screen");
-  statusElem.textContent = "Wake Lock is active!";
-} catch (err) {
-  // The Wake Lock request has failed - usually system related, such as battery.
-  statusElem.textContent = `${err.name}, ${err.message}`;
+async function wakeLock(){
+    if ("wakeLock" in navigator) {
+        isSupported = true;
+        console.log("Screen Wake Lock API supported");
+    } else {
+        wakeButton.disabled = true;
+        console.log("Wake lock is not supported by this browser.");
+    }
+
+    // Create a reference for the Wake Lock.
+    let wakeLock = null;
+
+    try {
+        wakeLock = await navigator.wakeLock.request("screen");
+        console.log("Wake Lock is active!");
+    } catch (err) {
+        // The Wake Lock request has failed - usually system related, such as battery.
+        console.log(`${err.name}, ${err.message}`);
+    }
 }
+
+wakeLock();
