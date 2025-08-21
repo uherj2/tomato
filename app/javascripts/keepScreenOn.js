89f5c82fa8969e61a60941ef88fafb7c54ed8787
Jack Uher <149542675+uherj2@users.noreply.github.com>
2025-08-21 15:48:14 -0400

    Create keepScreenOn.js

---
+++ app/javascripts/keepScreenOn.js
@@ -0,0 +1,19 @@
+if ("wakeLock" in navigator) {
+  isSupported = true;
+  statusElem.textContent = "Screen Wake Lock API supported!";
+} else {
+  wakeButton.disabled = true;
+  statusElem.textContent = "Wake lock is not supported by this browser.";
+}
+
+// Create a reference for the Wake Lock.
+let wakeLock = null;
+
+// create an async function to request a wake lock
+try {
+  wakeLock = await navigator.wakeLock.request("screen");
+  statusElem.textContent = "Wake Lock is active!";
+} catch (err) {
+  // The Wake Lock request has failed - usually system related, such as battery.
+  statusElem.textContent = `${err.name}, ${err.message}`;
+}
